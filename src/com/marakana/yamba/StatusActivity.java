899434cc7bd10bf8e0aec192ba2a3108c5c994b5
spirosbond <spirosbond@hotmail.com>
2013-07-02 21:25:38 +0300

    jar and permissions added

--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -1,22 +1,39 @@
 package com.marakana.yamba;
 
-import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 import android.os.Bundle;
-import android.view.Menu;
+import android.app.Activity;
+import winterwell.jtwitter.Twitter;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener { //
 
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+		// Find views
+		editText = (EditText) findViewById(R.id.editText); //
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this); //
+		twitter = new Twitter("student", "password"); //
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
-	}
+	// Called when button is clicked //
+	public void onClick(View v) {
 
+		twitter.setStatus(editText.getText().toString()); //
+		Log.d(TAG, "onClicked");
+	}
 }
