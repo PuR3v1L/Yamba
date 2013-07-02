ac05ce47bca5203768549d0ec66fa4eb644b2bfe
spirosbond <spirosbond@hotmail.com>
2013-07-02 22:18:44 +0300

    save

--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -1,39 +1,40 @@
 package com.marakana.yamba;
 
-import android.view.View.OnClickListener;
-import android.widget.EditText;
-import android.view.View;
-import android.widget.Button;
-import android.util.Log;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
 import winterwell.jtwitter.Twitter;
 
 public class StatusActivity extends Activity implements OnClickListener { //
 
-	private static final String TAG = "StatusActivity";
-	EditText editText;
-	Button updateButton;
-	Twitter twitter;
+    private static final String TAG = "StatusActivity";
+    EditText editText;
+    Button updateButton;
+    Twitter twitter;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_status);
-		// Find views
-		editText = (EditText) findViewById(R.id.editText); //
-		updateButton = (Button) findViewById(R.id.buttonUpdate);
-		updateButton.setOnClickListener(this); //
-		twitter = new Twitter("student", "password"); //
-		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+        // Find views
+        editText = (EditText) findViewById(R.id.editText); //
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        updateButton.setOnClickListener(this); //
+        twitter = new Twitter("student", "password"); //
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
 
-	// Called when button is clicked //
-	public void onClick(View v) {
+    // Called when button is clicked //
+    public void onClick(View v) {
 
-		twitter.setStatus(editText.getText().toString()); //
-		Log.d(TAG, "onClicked");
-	}
+        twitter.setStatus(editText.getText().toString()); //
+        Log.d(TAG, "onClicked");
+    }
 }
